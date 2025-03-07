9027fb182c607a3f0b9977c3a36ad34475bfd773
Padmini7890 <dasmanu7890@gmail.com>
2025-03-07 14:58:22 +0530

    Update SDG_Quiz.py

--- SDG_Quiz.py
+++ SDG_Quiz.py
@@ -4,7 +4,7 @@ import json
 # Sustainable Development Goals Data with images
 sdg_goals = [
     {"id": 1, "title": "No Poverty", "description": "End poverty in all its forms everywhere.",
-     ""D:\Padmini MSc. Prev\game\E_SDG_PRINT-01.jpg""},
+     "D:\Padmini MSc. Prev\game\E_SDG_PRINT-01.jpg"},
     {"id": 2, "title": "Zero Hunger", "description": "End hunger, achieve food security and improved nutrition.",
      "image": "https://upload.wikimedia.org/wikipedia/commons/4/42/SDG_logo_zero_hunger.png"},
     {"id": 3, "title": "Good Health and Well-being", "description": "Ensure healthy lives and promote well-being for all ages.",
